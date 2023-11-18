a4831e0b4fbb8a5099325e398bf9350f7ba3d58d
cardosojl <cardosojl@outlook.com.br>
2023-11-17 21:02:14 -0300

    removing .css

--- src/apis/siscopUpload.ts
+++ src/apis/siscopUpload.ts
@@ -1,7 +1,5 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { ObjFilter } from 'src/config/types/types';
-
-console.log(process.env.REACT_APP_SISCOP_API_UPLOAD);
 const axiosUpload = axios.create({ baseURL: process.env.REACT_APP_SISCOP_API_UPLOAD, withCredentials: true });
 
 export async function uploadCreate(baseUrl: string, body: ObjFilter, header: AxiosRequestConfig): Promise<AxiosResponse> {
